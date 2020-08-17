37545fb144d6e36a3722fcc92e1e46323cc43d7d
Unknown <omax1972@gmail.com>
2020-08-17 19:20:43 +0300

    Create project

---
+++ gradlew.bat
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+**
+** Copyright 2006, The Android Open Source Project
+**
+** Licensed under the Apache License, Version 2.0 (the "License"); 
+** you may not use this file except in compliance with the License. 
+** You may obtain a copy of the License at 
+**
+**     http://www.apache.org/licenses/LICENSE-2.0 
+**
+** Unless required by applicable law or agreed to in writing, software 
+** distributed under the License is distributed on an "AS IS" BASIS, 
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+** See the License for the specific language governing permissions and 
+** limitations under the License.
+*/
+-->
+<timezones>
+    <!-- ANDORRA, 1:00 -->
+
+    <timezone code="ad">Europe/Andorra</timezone>
+
+    <!-- UNITED ARAB EMIRATES, 4:00 -->
+
+    <timezone code="ae">Asia/Dubai</timezone>
+
+    <!-- AFGHANISTAN, 4:30 -->
+
+    <timezone code="af">Asia/Kabul</timezone>
+
+    <!-- ANTIGUA AND BARBUDA, -4:00 -->
+
+    <timezone code="ag">America/Antigua</timezone>
+
+    <!-- ANGUILLA, -4:00 -->
+
+    <timezone code="ai">America/Anguilla</timezone>
+
+    <!-- ALBANIA, 1:00 -->
+
+    <timezone code="al">Europe/Tirane</timezone>
+
+    <!-- ARMENIA, 4:00 -->
+
+    <timezone code="am">Asia/Yerevan</timezone>
+
+    <!-- ANGOLA, 1:00 -->
+
+    <timezone code="ao">Africa/Luanda</timezone>
+
+    <!-- ANTARCTICA, 12:00 -->
+
+    <timezone code="aq">Antarctica/McMurdo</timezone>
+
+    <!-- ANTARCTICA, 10:00 -->
+
+    <timezone code="aq">Antarctica/DumontDUrville</timezone>
+
+    <!-- ANTARCTICA, 8:00 -->
+
+    <timezone code="aq">Antarctica/Casey</timezone>
+
+    <!-- ANTARCTICA, 7:00 -->
+
+    <timezone code="aq">Antarctica/Davis</timezone>
+
+    <!-- ANTARCTICA, 5:00 -->
+
+    <timezone code="aq">Antarctica/Mawson</timezone>
+
+    <!-- ANTARCTICA, 6:00 -->
+
+    <timezone code="aq">Antarctica/Vostok</timezone>
+
+    <!-- ANTARCTICA, 3:00 -->
+
+    <timezone code="aq">Antarctica/Syowa</timezone>
+
+    <!-- ANTARCTICA, 0:00 -->
+
+    <timezone code="aq">Antarctica/Troll</timezone>
+
+    <!-- ANTARCTICA, -3:00 -->
+
+    <timezone code="aq">Antarctica/Rothera</timezone>
+
+    <!-- ANTARCTICA, -4:00 -->
+
+    <timezone code="aq">Antarctica/Palmer</timezone>
+
+    <!-- ARGENTINA, -3:00 -->
+
+    <timezone code="ar">America/
